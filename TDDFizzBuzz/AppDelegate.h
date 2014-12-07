dafa4bf70eca490e1eb225169707daab6b19af02
ountzza <pond1982a@gmail.com>
2014-12-07 23:06:01 +0700

    Initial Commit

---
+++ TDDFizzBuzz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TDDFizzBuzz
+//
+//  Created by Pondd on 12/7/2557 BE.
+//  Copyright (c) 2557 Pondd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TDDFizzBuzz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TDDFizzBuzz
+//
+//  Created by Pondd on 12/7/2557 BE.
+//  Copyright (c) 2557 Pondd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
